55f568d4d6033e508e7c2c659da2711fb05f199a
Samuel Adekunle <me@samuel-adekunle.software>
2022-06-04 11:53:03 +0100

    try to fail test

--- __tests__/index.test.jsx
+++ __tests__/index.test.jsx
@@ -7,7 +7,7 @@ describe('Home', () => {
         render(<Home />)
 
         const heading = screen.getByRole('heading', {
-            name: /welcome to next\.js!/i,
+            name: /not welcome to next\.js!/i,
         })
 
         expect(heading).toBeInTheDocument()
